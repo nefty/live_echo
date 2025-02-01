7b6c3045aa22902441615faa2c7b20009a1957a8
Brian Eft <brianeft@gmail.com>
2025-01-31 21:31:49 -0500

    increase SDP candidates timeout

--- lib/live_echo/pipeline.ex
+++ lib/live_echo/pipeline.ex
@@ -13,10 +13,12 @@ defmodule LiveEcho.Pipeline do
 
   @impl true
   def handle_init(_ctx, %{channel: channel}) do
+    timestamp = DateTime.utc_now() |> Calendar.strftime("%Y%m%d_%H%M%S")
     spec =
       [
         child(:webrtc, %WebRTC.Source{
-          signaling: channel
+          signaling: channel,
+          sdp_candidates_timeout: Membrane.Time.seconds(3)
         }),
         child(:matroska, Membrane.Matroska.Muxer),
         get_child(:webrtc)
@@ -27,7 +29,7 @@ defmodule LiveEcho.Pipeline do
         |> via_out(:output, options: [kind: :video])
         |> get_child(:matroska),
         get_child(:matroska)
-        |> child(:sink, %Membrane.File.Sink{location: "recording.mkv"})
+        |> child(:sink, %Membrane.File.Sink{location: "recording_#{timestamp}.mkv"})
       ]
 
     {[spec: spec], %{}}
